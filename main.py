99de90a980b9dc5e227001c4efd12958f4ce0641
SeanMcReynolds <sean.mcreynolds066@gmail.com>
2023-10-23 12:31:39 -0500

    not finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen
 from PIL import Image
 
@@ -6,6 +7,32 @@ class PhotoEditorApp(App):
     pass
 
 class Display(Screen):
+    # coordinates = []
+    # def on_touch_down(self, touch):
+    #     x, y = touch.x, touch.y
+    #     self.coordinates.append(int(x))
+    #     self.coordinates.append(int(y))
+    #     if len(self.coordinates) > 6:
+    #         self.coordinates = self.coordinates[2:]
+    #     touch.push()
+    #     touch.apply_transform_2d(self.to_local)
+    #     ret = super(RelativeLayout, self).on_touch_down(touch)
+    #     touch.pop()
+    #     return ret
+    #
+    # def on_touch_up(self, touch):
+    #     x, y = touch.x, touch.y
+    #     self.coordinates.append(int(x))
+    #     self.coordinates.append(int(y))
+    #     if len(self.coordinates) > 6:
+    #         self.coordinates = self.coordinates[2:]
+    #     touch.push()
+    #     touch.apply_transform_2d(self.to_local)
+    #     ret = super(RelativeLayout, self).on_touch_up(touch)
+    #     touch.pop()
+    #     return ret
+    #
+    #
     def load(self):
         self.ids.image.source = self.ids.image_load.text
 
@@ -67,6 +94,23 @@ class Display(Screen):
                     pixels[x, y] = (200, 100, 200)
         image.save("vertical_lines.png")
         self.ids.image.source = "vertical_lines.png"
+
+    def pixelate(self):
+        image = Image.open(self.ids.image.source)
+        pixels = image.load()
+        x1,y1 = 500,500
+        x2,y2 = 600,600
+        size = 20
+        for x in range(x1, x2, size):
+            for y in range(y1, y2, size):
+                red, green, blue = pixels[x, y]
+                for newx in range(x, min(x+size, x2)):
+                    for newy in range(y, min(y+size, y2)):
+                        pixels[newx, newy] = (red, green, blue)
+        image.save("pixelate.jpg")
+        self.ids.image.source = "pixelate.jpg"
+
+
 
 #self.ids.image.source = self.display_images()
 
